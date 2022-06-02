749233436241b7715e6625e97224e367acac602b
Arunava-Barua <rahulbarua31@gmail.com>
2022-06-02 21:08:10 +0530

    Create Input and Create NFT page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef, useContext } from 'react';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import { makeId } from '../utils/makeId';
 
 import images from '../assets';
@@ -59,7 +59,7 @@ const Home = () => {
 
           <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {[5, 6, 7, 8, 9, 10].map((creator, i) => (
+              {[4, 5, 6, 7, 8, 9, 10].map((creator, i) => (
                 <CreatorCard
                   key={`creator-${i}`}
                   rank={i + 1}
@@ -81,6 +81,32 @@ const Home = () => {
               )}
             </div>
           </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+
+            <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+              Search
+            </div>
+          </div>
+          <div className="flex-1 mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i: i + 1,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.543).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(3)}`,
+                  owner: `0x${makeId(3)}...${makeId(3)}`,
+                  description: 'Cool NFT for sale',
+                }}
+              />
+            ))}
+          </div>
+
         </div>
       </div>
     </div>
